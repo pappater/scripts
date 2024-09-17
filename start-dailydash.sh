0d51e100d624fdfb4a6379ce2b5d473861831d2e
pappater <pratheesheswaran@gmail.com>
2024-09-17 09:04:55 +0530

    updated the script to open the vs code post starting up the server

--- start-dailydash.sh
+++ start-dailydash.sh
@@ -3,10 +3,15 @@
 # Navigate to the client folder and run the dev command
 cd /Users/pratheesh/Documents/dailydash/dailydash-client/
 npm run dev &
+code . &
 
 # Navigate to the server folder and run the dev command
 cd /Users/pratheesh/Documents/dailydash/dailydash-server/
 npm run dev &
+code . &
+
+
+
 
 # Open the local client in the default browser (optional)
 open http://localhost:5173
